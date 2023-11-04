3959ba7e39e9afcdbea05a162795d1da57b156e6
EdwardTi <edwardti2004@gmail.com>
2023-11-05 03:24:47 +0800

    lab5d display employees in a given department

--- employee_info.py
+++ employee_info.py
@@ -33,7 +33,9 @@ def calculate_average_salary():
 def get_employees_by_dept(department):
     result = []
 
-    # Add your implementation from here
+    for item in employee_data:
+        if item["department"] == department:
+            result.append(item)
 
 
     return result
